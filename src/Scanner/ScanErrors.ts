9839632b8255d44aa637e44a18ce91a4a903c4d3
Geomitron <22552797+Geomitron@users.noreply.github.com>
2020-08-28 11:54:01 -0400

    Write files with zero errors

--- src/Scanner/ScanErrors.ts
+++ src/Scanner/ScanErrors.ts
@@ -15,45 +15,43 @@ export const scanErrors: ChartError[] = []
  * Prints the list of library issues that were generated as a result of the scan.
  */
 export async function saveAllErrors() {
-  if (scanErrors.length > 0) {
-    for (const error of scanErrors) {
-      if (error.chartText == undefined) {
-        error.chartText = error.chart.isArchive ? error.chart.files[0].name : error.chart.folderName
-      }
+  for (const error of scanErrors) {
+    if (error.chartText == undefined) {
+      error.chartText = error.chart.isArchive ? error.chart.files[0].name : error.chart.folderName
+    }
+  }
+
+  const sourceDriveIDs = [...new Set(scanErrors.map(error => error.chart.source.sourceDriveID))]
+
+  for (const sourceDriveID of sourceDriveIDs) {
+    const driveErrors = scanErrors.filter(error => error.chart.source.sourceDriveID == sourceDriveID)
+    let driveErrorText = ''
+
+    const folderIDs = [...new Set(driveErrors.map(error => error.chart.folderID))]
+
+    for (const folderID of folderIDs) {
+      const chartErrors = driveErrors.filter(error => error.chart.folderID == folderID)
+
+      const headerText = `["${chartErrors[0].chart.folderName}" at https://drive.google.com/drive/folders/${folderID}]\n`
+      const errorText = chartErrors.map(error => `[${RegularErrorTypes[error.type] ?? SeriousErrorTypes[error.type]
+          }] for ${error.chartText}: ${error.description}`).join('\n')
+      driveErrorText += headerText + errorText + '\n\n'
     }
 
-    const sourceDriveIDs = [...new Set(scanErrors.map(error => error.chart.source.sourceDriveID))]
+    let errorFolder: string
+    const seriousErrors = driveErrors.filter(error => error.type >= ERROR_TYPE_BORDER)
 
-    for (const sourceDriveID of sourceDriveIDs) {
-      const driveErrors = scanErrors.filter(error => error.chart.source.sourceDriveID == sourceDriveID)
-      let driveErrorText = ''
-
-      const folderIDs = [...new Set(driveErrors.map(error => error.chart.folderID))]
-
-      for (const folderID of folderIDs) {
-        const chartErrors = driveErrors.filter(error => error.chart.folderID == folderID)
-
-        const headerText = `["${chartErrors[0].chart.folderName}" at https://drive.google.com/drive/folders/${folderID}]\n`
-        const errorText = chartErrors.map(error => `[${RegularErrorTypes[error.type] ?? SeriousErrorTypes[error.type]
-            }] for ${error.chartText}: ${error.description}`).join('\n')
-        driveErrorText += headerText + errorText + '\n\n'
-      }
-
-      let errorFolder: string
-      const seriousErrors = driveErrors.filter(error => error.type >= ERROR_TYPE_BORDER)
-
-      if (driveErrors.length == 0) {
-        errorFolder = NO_ERRORS_PATH
-      } else if (seriousErrors.length >= scanSettings.seriousErrorThreshold) {
-        errorFolder = MANY_ERRORS_PATH
-      } else {
-        errorFolder = FEW_ERRORS_PATH
-      }
-
-      const errorPath = join(errorFolder, sanitizeFilename(driveErrors[0].chart.source.sourceName) + '.txt')
-
-      await writeFile(errorPath, driveErrorText, { flag: 'a' })
+    if (driveErrors.length == 0) {
+      errorFolder = NO_ERRORS_PATH
+    } else if (seriousErrors.length >= scanSettings.seriousErrorThreshold) {
+      errorFolder = MANY_ERRORS_PATH
+    } else {
+      errorFolder = FEW_ERRORS_PATH
     }
+
+    const errorPath = join(errorFolder, sanitizeFilename(driveErrors[0].chart.source.sourceName) + '.txt')
+
+    await writeFile(errorPath, driveErrorText, { flag: 'a' })
   }
 }
 
