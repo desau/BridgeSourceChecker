0462a16e188328075e895160824fa9edd7731fe8
Geomitron <22552797+Geomitron@users.noreply.github.com>
2021-02-20 19:19:53 -0500

    Updated node-unrar-js

--- src/Download/RarExtractor.ts
+++ src/Download/RarExtractor.ts
@@ -2,7 +2,6 @@ import * as fs from 'fs'
 import { join } from 'path'
 import * as unrarjs from 'node-unrar-js'
 import { promisify } from 'util'
-import { log } from '../UtilFunctions'
 
 const mkdir = promisify(fs.mkdir)
 const open = promisify(fs.open)
@@ -13,37 +12,36 @@ const close = promisify(fs.close)
  * Extracts a .rar archive found at `sourceFile` and puts the extracted results in `destinationFolder`.
  */
 export async function extractRar(sourceFile: string, destinationFolder: string) {
-  const extractor = unrarjs.createExtractorFromFile(sourceFile, destinationFolder)
+  const extractor = await unrarjs.createExtractorFromFile({ filepath: sourceFile, targetPath: destinationFolder })
 
   const fileList = extractor.getFileList()
 
-  if (fileList[0].state != 'FAIL') {
+  // Iterate through headers and save them to another array (because unrarjs didn't feel like allowing an iterator to iterate twice)
+  const headers = fileList.fileHeaders
+  const savedHeaders = []
+  for (const header of headers) {
+    savedHeaders.push(header)
+  }
 
-    // Create directories for nested archives (because unrarjs didn't feel like handling that automatically)
-    const headers = fileList[1].fileHeaders
-    for (const header of headers) {
-      if (header.flags.directory) {
-        try {
-          await mkdir(join(destinationFolder, header.name), { recursive: true })
-        } catch (e) {
-          throw new Error(`Failed to extract directory: ${e}`)
-        }
+  // Create directories for nested archives (because unrarjs didn't feel like handling that automatically)
+  for (const header of savedHeaders) {
+    if (header.flags.directory) {
+      try {
+        await mkdir(join(destinationFolder, header.name), { recursive: true })
+      } catch (e) {
+        throw new Error(`Failed to extract directory: ${e}`)
       }
     }
-  } else {
-    log.warn('Warning: failed to read .rar files: ', fileList[0].reason, fileList[0].msg)
   }
 
   // Extract archive
-  const extractResult = extractor.extractAll()
-
-  if (extractResult[0].state == 'FAIL') {
-    throw new Error(`${extractResult[0].reason}: ${extractResult[0].msg}`)
+  const test = extractor.extract()
+  for (const file of test.files) {
+    // Iterate through files and do nothing (because unrarjs didn't feel like extracting when .extract() was called)
   }
 
   // Set file modification times (because unrarjs didn't feel like handling that automatically)
-  const headers = fileList[1].fileHeaders
-  for (const header of headers) {
+  for (const header of savedHeaders) {
     if (!header.flags.directory) {
       try {
         const fd = await open(join(destinationFolder, header.name), 'r+')
